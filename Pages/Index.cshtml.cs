9c70ffeb2f152a81233a3e205e25b6ccb635bac2
AhmedMAyoub <ahmed2000mostafa@gmail.com>
2021-08-09 04:10:26 +0200

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace RazorAPI.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Colour { get; set; }
+        public int Doors { get; set; }
+        public int Price { get; set; }
+    }
+}
