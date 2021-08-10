91cf6e826bb2a2f32bde855b8c21e2fe3dc43dd3
AhmedMAyoub <ahmed2000mostafa@gmail.com>
2021-08-10 05:20:27 +0200

    Formatting

--- Controllers/CarController.cs
+++ Controllers/CarController.cs
@@ -10,39 +10,46 @@ namespace RazorAPI.Controllers
     public class CarController : Controller
     {
         private readonly ICarService _carService;
+
         public CarController(ICarService carService)
         {
             _carService = carService;
         }
+
         // GET: api/Car
         [HttpGet]
         public List<Car> Get()
         {
             return _carService.ReadAll();
         }
+
         // GET: api/Car/5
         [HttpGet("{id}", Name = "Get")]
         public Car Get(int id)
         {
             return _carService.Read(id);
         }
+
         // POST: api/Car
         [HttpPost]
         public void Post([FromBody] Car car)
         {
             _carService.Create(car);
         }
+
         // PUT: api/Car/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] Car car)
         {
             _carService.Update(car);
         }
+
         // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
             _carService.Delete(id);
         }
+        
     }
 }
